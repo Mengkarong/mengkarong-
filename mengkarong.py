294306ec937fdd905c4c21e631bd8be4fc55c7d7
Mengkarong <122902864+Mengkarong@users.noreply.github.com>
2023-01-18 01:32:04 +0700

    Add files via upload

---
+++ mengkarong.py
@@ -0,0 +1,12 @@
+import random
+import sys
+import time
+def mengetik(s):
+    for c in   s + '/n' :
+         sys.stdout write(c)
+         sys.stdout plush()
+
+         time,sleep(ramdom.random() • 0.2)
+
+
+mengetik('') mengkarong √√.
